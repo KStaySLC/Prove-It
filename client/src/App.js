3990a96bcb344ae35f3a1ae0ab0a73e32731ca58
Kammin Avery <kammin.avery@gmail.com>
2022-09-27 18:53:16 -0600

    messed with app, hope it works

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './views/layout';
+import Login from './components/login';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+       <Route path='/' element={<Layout><Login /></Layout>}/>
+       <Route path = '/Login' element = {<Layout><Login/></Layout>} />
+      </Routes>
     </div>
   );
 }
