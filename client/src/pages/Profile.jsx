91acb3a2586640e11ae65a366fa7864a5b1bafbc
Grant Register <register.grant@gmail.com>
2022-09-29 12:34:18 -0600

    very little but was on main branch

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import KStaySLC from '../images/KStaySLC.jfif'
-import '../static/profile.css'
 
 function Profile() {
   return (
     <div>
-      <header>
-      </header>
       <div class="profileContainer">Look At My Cool Shades.</div>
-        <img src={KStaySLC} alt="" />
+
 
     </div>
 
