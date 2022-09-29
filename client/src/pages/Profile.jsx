2433bf4fae2bc04d0b386399135a30f1649575a2
Grant Register <register.grant@gmail.com>
2022-09-29 10:13:50 -0600

    added an images folder for profile page and did some profile.jsx work

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import KStaySLC from '../images/KStaySLC.jfif'
+import '../static/profile.css'
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+      <header>
+      </header>
+      <div class="profileContainer">Look At My Cool Shades.</div>
+        <img src={KStaySLC} alt="" />
+
+    </div>
+
   )
 }
 
