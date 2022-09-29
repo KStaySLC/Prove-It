7783a1c9e7297dac35706c6f55f6abc1bc086613
Grant Register <register.grant@gmail.com>
2022-09-29 13:56:12 -0600

    profile page and styling - needs an expert

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React from "react";
+import "../static/profile.css";
+import imgKA from "../images/imgKA.jfif";
+const width = { width: "15%" };
 
 function Profile() {
   return (
     <div>
-      <div class="profileContainer">Look At My Cool Shades.</div>
-
-
+      <div class="profileContainer">
+        <img src={imgKA} alt="Hey Everybody!" /> 
+        <p class="postNumber">Total Posts</p>
+        <p class="amount">55</p>
+      </div>
+      <div class="posts"></div>
+      <button class="addPost" type="button" style={width}>
+        Add Post
+      </button>
+      <button className="seePosts" type="button" style={width}>
+        See My Posts
+      </button>
+      <div class="postHistory">
+        <div className="postHistoryHeader"></div>
+        <h1>Post History or Maybe Feed</h1>
+        <p>My Last Post</p>
+        <p>My Last Post Before My Last Post</p>
+        <p>My Penultimate Post Before My Penultimate Post</p>
+        <p>My Last Post Before My Penultimate Penultimate Post</p>
+        <p>
+          My Penultimate Post Before My Last Post Before My Penultimate
+          Penultimate Post
+        </p>
+        <p>
+          My Penultimate Penultimate Post Before My Last Post Before My
+          Penultimate Penultimate Post. Say Penultimate Again. I Dare You.
+        </p>
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default Profile
+export default Profile;
