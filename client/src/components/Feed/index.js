a2addeef1ef43d3692ccce48c3f441db6d6fa0a7
Grant Register <register.grant@gmail.com>
2022-09-29 20:47:03 -0600

    post form and feed components

--- client/src/components/Feed/index.js
+++ client/src/components/Feed/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Feed() {
+const Feed = ({ posts, title }) => {
+  if (!posts.length) {
+    return <h3>No Posts Yet</h3>;
+  }
+
   return (
-    <div>Feed</div>
-  )
-}
+    <div>
+      <h3>{title}</h3>
+      {posts &&
+        posts.map((post) => (
+          <div key={post._id} className="card mb-3">
+            <h4 className="card-header bg-primary text-light p-2 m-0">
+              {post.postAuthor} <br />
+              <span style={{ fontSize: '1rem' }}>
+                had this thought on {post.createdAt}
+              </span>
+            </h4>
+            <div className="card-body bg-light p-2">
+              <p>{post.postText}</p>
+            </div>
+            <Link
+              className="btn btn-primary btn-block btn-squared"
+              to={`/posts/${post._id}`}
+            >
+              Join the discussion on this thought.
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
 
-export default Feed
+export default Feed;
