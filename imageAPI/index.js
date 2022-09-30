ff486555477ce22e375854f1af178d2b11a64ca4
Kevin Stayner <kstayslc@icloud.com>
2022-09-29 19:35:59 -0600

    Debugging

--- imageAPI/index.js
+++ imageAPI/index.js
@@ -27,11 +27,8 @@ const multer = Multer({
 
 let projectId = "secure-petal-357801"; // Get this from Google Cloud
 let keyFilename = "mykey.json"; // Get this from Google Cloud -> Credentials -> Service Accounts
-const storage = new Storage({
-  projectId,
-  keyFilename,
-});
-const bucket = storage.bucket("resume-builder-uofu"); 
+
+
 const upload = async (req, res) => {
   try {
     await processFile(req, res);
@@ -125,38 +122,6 @@ module.exports = {
   download,
 };
 
-// Get this from Google Cloud -> Storage
-
-// // // // Gets all files in the defined bucket
-// app.get("/upload", async (req, res) => {
-//   try {
-//     const [files] = await bucket.getFiles();
-//     res.send([files]);
-//     console.log("Success");
-//   } catch (error) {
-//     res.send("Error:" + error);
-//   }
-// });
-// // // Streams file upload to Google Storage
-
-// app.post("/upload", multer.single("imgfile"), (req, res) => {
-//   console.log("Made it /upload");
-//   try {
-//     if (req.file) {
-//       console.log("File found, trying to upload...");
-//       const blob = bucket.file(req.file.originalname);
-//       const blobStream = blob.createWriteStream();
-
-//       blobStream.on("finish", () => {
-//         res.status(200).send("Success");
-//         console.log("Success");
-//       });
-//       blobStream.end(req.file.buffer);
-//     } else throw "error with img";
-//   } catch (error) {
-//     res.status(500).send(error);
-//   }
-// });
 
 // // Get the main index html file
 app.get("/", (req, res) => {
