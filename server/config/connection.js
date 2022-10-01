1356bc574b23c1e84824894b804e850876e30baf
Kevin Stayner <kstayslc@icloud.com>
2022-10-01 13:06:04 -0600

    Modifications to package lock and package for Heroku issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/prove-it', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Proveitstuff:password12345@Cluster0.6g2qrxw.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
